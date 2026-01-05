2c989a7e36244f21256da8c8d38327410a55f05a
Adelando <adelando@users.noreply.github.com>
2026-01-05 17:18:15 +0800

    Update update.py

--- custom_components/pi-hole-stats/update.py
+++ custom_components/pi-hole-stats/update.py
@@ -1,15 +1,3 @@
-from homeassistant.components.update import UpdateEntity, UpdateEntityFeature
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from .const import DOMAIN
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([
-        PiHoleUpdateEntity(coordinator, entry, "core", "Core"),
-        PiHoleUpdateEntity(coordinator, entry, "ftl", "FTL"),
-        PiHoleUpdateEntity(coordinator, entry, "web", "Web Interface"),
-    ])
-
 class PiHoleUpdateEntity(CoordinatorEntity, UpdateEntity):
     _attr_supported_features = UpdateEntityFeature.RELEASE_NOTES
 
@@ -19,7 +7,7 @@ class PiHoleUpdateEntity(CoordinatorEntity, UpdateEntity):
         self._attr_name = f"Pi-hole {name} Update"
         self._attr_unique_id = f"{entry.entry_id}_update_{key}"
         self._attr_device_info = coordinator.device_info
-        self._attr_release_url = "https://github.com/pi-hole/pi-hole/releases"
+        self._attr_release_url = f"https://github.com/pi-hole/{'web' if key=='web' else 'pi-hole'}/releases"
 
     @property
     def installed_version(self):
@@ -27,6 +15,4 @@ class PiHoleUpdateEntity(CoordinatorEntity, UpdateEntity):
 
     @property
     def latest_version(self):
-        current = self.installed_version
-        has_update = self.coordinator.data.get(f"up_{self._key}")
-        return f"{current}-update" if has_update else current
+        return self.coordinator.data.get(f"rem_{self._key}")
