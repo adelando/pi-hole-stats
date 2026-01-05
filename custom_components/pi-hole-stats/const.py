4c4af4dc4a79e55f3e7870b626bc2f02dd57298b
Adelando <adelando@users.noreply.github.com>
2026-01-05 16:44:30 +0800

    Update const.py

--- custom_components/pi-hole-stats/const.py
+++ custom_components/pi-hole-stats/const.py
@@ -1,3 +1,13 @@
+"""Constants for the Pi-hole v6 Stats integration."""
+
 DOMAIN = "pi_hole_stats"
-CONF_LOCATION = "location"
-DEFAULT_NAME = "Pi-Hole"
+NAME = "Pi-hole v6 Stats"
+
+DEFAULT_PORT = 80
+
+DEFAULT_UPDATE_INTERVAL = 5 # Seconds
+
+# Attribute Keys
+ATTR_HOT_LIMIT = "hot_limit"
+ATTR_MSG_LIST = "msg_list"
+ATTR_HOST_DETAILS = "host_attr"
